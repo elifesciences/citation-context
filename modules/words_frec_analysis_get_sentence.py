7a5f978c2fb3e2260b2ef0943281558675b66215
Daniel Ecer <de-code@users.noreply.github.com>
2017-11-09 18:17:17 +0000

    fixed indent

--- modules/words_frec_analysis_get_sentence.py
+++ modules/words_frec_analysis_get_sentence.py
@@ -8,14 +8,14 @@ from copy import deepcopy
 
 
 def filter_sentence(citing_sentence):
-    
+
     """
 	It removes citations, html labels and stop words
     """
 
     if citing_sentence == None:
         return " " #filtered_sentences_noNone.append(" ")
-    
+
     if citing_sentence != None:
         citing_sentence = re.sub("[\<\[].*?[\>\]]", "", citing_sentence) #to remove citations
         #citing_sentence = re.sub("[*?]", "", citing_sentence) #to remove citations
@@ -39,34 +39,34 @@ def analysis(citing_sentences_list):
     citing_sentences_original = deepcopy(citing_sentences_list) # This is a list of lists, so you need deepcopy. We are keeping a copy because we are going to modify = citing_sentences_list
     filtered_sentences_list = citing_sentences_list.apply(filter_sentence)
     print(filtered_sentences_list)
-    
+
     count_vectorizer = CountVectorizer(max_features=5) # We will check the X most  frequent words  (max_features = X)
-   # count_vectorizer = CountVectorizer(max_features) 
+    # count_vectorizer = CountVectorizer(max_features) 
     count_vectorizer.fit_transform(filtered_sentences_list) #backToBytes
-    
+
     try:
         frequent_words = count_vectorizer.get_feature_names()
         print(frequent_words)
     except ValueError:
         print("No sentences to analyse") #It may happen that we hava filetered out all the words in the sentence!!!
         return
-        
-    
+
+
     count_vectors = count_vectorizer.transform(filtered_sentences_list)
     word_frequency = count_vectors.toarray()
-    
+
     number_words_in_sentence = np.sum(count_vectors.toarray(),axis=1).tolist()
-    
+
     print("Frequent words included: ", number_words_in_sentence)
     print("Maximum words in the same sentence  = ", max(number_words_in_sentence))
-    
+
     sentence_length = []
     for sentence in citing_sentences_list:
         if sentence == None:
             sentence_length.append(0)
         if sentence != None:
             sentence_length.append(len(sentence))
-    
+
     df_sentences = pd.DataFrame({"number_frequent_words" : number_words_in_sentence,
                                     "citing_sentence" : citing_sentences_original,
                                     "citing_sentence_filtered" : citing_sentences_list, 
@@ -93,14 +93,14 @@ def analysis(citing_sentences_list):
 
 
 def filter_sentence_technicisms(citing_sentence):
-    
+
     """
     It also removes technicism by checking if the words are in a list of about 2e(5) English words
     """
-    
+
     if citing_sentence == None:
         return " " #filtered_sentences_noNone.append(" ")
-    
+
     if citing_sentence != None:
         citing_sentence = re.sub("[\<\[].*?[\>\]]", "", citing_sentence) #to remove citations
         #citing_sentence = re.sub("[*?]", "", citing_sentence) #to remove citations
@@ -120,41 +120,42 @@ def filter_sentence_technicisms(citing_sentence):
 
 
 def analysis_nolimit(citing_sentences_list):
-	"""
-	Similar to .analysis, but there is no limit of words studied.
     """
-	
-    citing_sentences_original = deepcopy(citing_sentences_list) # This is a list of lists, so you need deepcopy. We are keeping a copy because we are going to modify = citing_sentences_list
+    Similar to .analysis, but there is no limit of words studied.
+    """
+
+    # This is a list of lists, so you need deepcopy. We are keeping a copy because we are going to modify = citing_sentences_list
+    citing_sentences_original = deepcopy(citing_sentences_list)
     filtered_sentences_list = citing_sentences_list.apply(filter_sentence_technicisms)
     #print(filtered_sentences_list)
     
     count_vectorizer = CountVectorizer(max_features=100) # We will check the X most  frequent words  (max_features = X)
-   # count_vectorizer = CountVectorizer(max_features) 
+    # count_vectorizer = CountVectorizer(max_features) 
     count_vectorizer.fit_transform(filtered_sentences_list) #backToBytes
-    
+
     try:
         frequent_words = count_vectorizer.get_feature_names()
         print(frequent_words)
     except ValueError:
         print("No sentences to analyse") #It may happen that we hava filetered out all the words in the sentence!!!
         return
-        
-    
+
+
     count_vectors = count_vectorizer.transform(filtered_sentences_list)
     word_frequency = count_vectors.toarray()
-    
+
     number_words_in_sentence = np.sum(count_vectors.toarray(),axis=1).tolist()
     
     #print("Frequent words included: ", number_words_in_sentence)
     #print("Maximum words in the same sentence  = ", max(number_words_in_sentence))
-    
+
     sentence_length = []
     for sentence in citing_sentences_list:
         if sentence == None:
             sentence_length.append(0)
         if sentence != None:
             sentence_length.append(len(sentence))
-    
+
     df_sentences = pd.DataFrame({"number_frequent_words" : number_words_in_sentence,
                                     "citing_sentence" : citing_sentences_original,
                                     "citing_sentence_filtered" : citing_sentences_list, 
@@ -164,35 +165,33 @@ def analysis_nolimit(citing_sentences_list):
     # We pick the sentences that includes the maximum number of frequent words. In case there are several such sentences we choose the shortes one.
     sentences_toCheck = df_sentences[(df_sentences["number_frequent_words"] == max(df_sentences["number_frequent_words"]))] #['sentence_citing_intro']
     min_length = min(sentences_toCheck["sentence_length"][:]) 
-    
+
     sentence = sentences_toCheck[sentences_toCheck['sentence_length'] == min_length]["citing_sentence"]
-    
+
     # To print the full content
     import sys
     sentence.to_csv(sys.stdout)
-    
+
     return frequent_words
-    
-    
-    
+
+
+
 
 def analyse_bigrams(citing_sentences_list):
     """
     Most frequent bi-grams.
     """
-    
+
     vectorizer = CountVectorizer()
-    
+
     citing_sentences_original = deepcopy(citing_sentences_list) # This is a list of lists, so you need deepcopy. We are keeping a copy because we are going to modify = citing_sentences_list
     filtered_sentences_list = citing_sentences_list.apply(filter_sentence_technicisms)
-    
-    
+
+
     bigram_vectorizer = CountVectorizer(ngram_range=(2, 2))
-    
+
     X_2 = bigram_vectorizer.fit_transform(filtered_sentences_list).toarray()
     vocab = bigram_vectorizer.vocabulary_
     count_values = X_2.sum(axis=0)
     counts = sorted([(count_values[i],k) for k,i in vocab.items()], reverse=True)
     return counts
-    
-    
